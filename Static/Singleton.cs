1efae60581c60dcd1ffe0ef9e69bf878f768aefd
Silas Peters <silaspeters03@gmail.com>
2023-09-21 15:01:59 +0200

    [BUG] Singleton was incorrect

--- Static/Singleton.cs
+++ Static/Singleton.cs
@@ -4,11 +4,21 @@ namespace Unity_Essentials.Static
 {
 	public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 	{
-		protected abstract void Awake();
-
-		public static void Forget() => _instance = null;
+		protected virtual void Awake()
+		{
+			_instance = null;
+			print("Forgot instance");
+		}
 
 		private static T _instance;
-		public static T Instance => _instance ??= FindObjectOfType<T>();
+		public static T Instance
+		{
+			get {
+				if (_instance != null) return _instance;
+				var found = FindObjectOfType<T>();
+				_instance = found;
+				return _instance;
+			}
+		}
 	}
 }
