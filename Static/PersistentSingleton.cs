3d5d76857a64ab4509949d704dd3e7652cab9fb7
Silas Peters <silaspeters03@gmail.com>
2023-09-24 13:09:21 +0200

    [REMOVE] PersistentSingleton is not done yet

--- Static/PersistentSingleton.cs
+++ Static/PersistentSingleton.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 namespace Unity_Essentials.Static
 {
-	public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+	// TODO remove internal modifier, and fix this class. Should it be a (abstract) component?
+	// https://gamedevbeginner.com/singletons-in-unity-the-right-way/
+	internal abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
 	{
 		public new static T Instance
 		{
@@ -17,3 +19,4 @@ namespace Unity_Essentials.Static
 		}
 	}
 }
+>>>>>>> Stashed changes
